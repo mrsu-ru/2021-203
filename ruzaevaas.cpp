628b5b6482f365c88e0b3f1eccc74fc447f6c81d
linkarull628 <84841363+linkarull628@users.noreply.github.com>
2021-06-03 18:09:27 +0300

    Update ruzaevaas.cpp

--- ruzaevaas.cpp
+++ ruzaevaas.cpp
@@ -14,6 +14,53 @@ void ruzaevaas::lab1()
  */
 void ruzaevaas::lab2()
 {
+double** arr_help = new double*[N];
+    for (int i = 0; i < N; i++)
+        arr_help[i] = new double[N + 1];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            arr_help[i][j] = A[i][j];
+    }
+
+    for (int i = 0; i < N; i++)
+        arr_help[i][N] = b[i];
+
+    for (int i = 0; i < N; i++) {
+        int max = i;
+        for (int j = i + 1; j < N; j++) {
+            if (abs(arr_help[j][i]) > abs(arr_help[max][i]))
+                max = j;
+        }
+        if (max != i) {
+            for (int k = 0; k < N + 1; k++)
+                swap(arr_help[i][k], arr_help[max][k]);
+        }
+
+        double helpel = arr_help[i][i];
+        for (int j = i; j < N + 1; j++)
+            arr_help[i][j] /= helpel;
+
+        for (int j = i + 1; j < N; j++) {
+            helpel = arr_help[j][i];
+            arr_help[j][i] = 0;
+            for (int k = i + 1; k < N + 1; k++)
+                arr_help[j][k] -= arr_help[i][k] * helpel;
+        }
+    }
+    int n = N + 1;
+    for (int i = N - 1; i > 0; i--) {
+        double h = 0;
+        for (int j = i + 1; j < n; j++) {
+            double help_el = arr_help[i - 1][j - 1];
+            arr_help[i - 1][j - 1] = 0;
+            h += arr_help[j - 1][n - 1] * help_el;
+        }
+        arr_help[i - 1][n - 1] = arr_help[i - 1][n - 1] - h;
+    }
+
+    for (int i = 0; i < N; i++)
+        x[i] = arr_help[i][N];
 
 }
 
